df99381347261ae8e4f2c25ae62224956e705656
hondaYoshitaka <yhonda1001@gamil.com>
2012-12-12 15:18:31 +0900

    タブの切り替えを出来るようにした

--- js/think-back.js
+++ js/think-back.js
@@ -2,8 +2,13 @@ $(function(){
 	$('.nav-list').find('li').each(function(){
 		var li = $(this);
 		li.find('a').on('click', function() {
+			var a = $(this);
+			a.tab('show');
+
 			$('.nav-list').find('li').removeClass('active');
 			li.addClass('active');
 		});
 	});
+
+	$("a[href='#count']").tab('show');
 });
